113948c577c22d8d1b5a9925e707b2229baee083
alinadir44 <56518209+alinadir44@users.noreply.github.com>
2019-11-09 17:21:05 +0500

    Update Module1.vb
    Revised assignment program

--- Module1.vb
+++ Module1.vb
@@ -1,5 +1,5 @@
 ﻿Module Module1
-    Structure Memrec
+     Structure Memrec
         <VBFixedString(25)> Dim Name As String
         Dim MemID As Integer
     End Structure
@@ -16,42 +16,51 @@
 
     Dim s1 As Memrec
 
+
     Sub recadder()
+        Dim ubound As Integer
         Dim recno As Integer
-        For x = 1 To 5
+        Console.WriteLine("How many records do you want to create?")
+        ubound = Console.ReadLine()
+
+        FileOpen(1, "E:\memberdata", OpenMode.Random, OpenAccess.ReadWrite, , Len(s1))
+
+        For x = 1 To ubound
             Console.WriteLine("Enter name of member")
             s1.Name = Console.ReadLine()
             Console.WriteLine("Enter ID of member")
             s1.MemID = Console.ReadLine()
 
-            FileOpen(1, "D:\memberdata", OpenMode.Random, Len(s1))
             recno = recno + 1
-            FilePut(1, s1, recno)
-            FileClose(1)
-        Next
 
+            FilePut(1, s1, recno)
+
+        Next
+        FileClose(1)
     End Sub
 
     Sub outputrec()
-        Dim s1 As Memrec
-        FileOpen(1, "D:\memberdata", OpenMode.Random, Len(s1))
+
+
+        FileOpen(1, "E:\memberdata", OpenMode.Random, OpenAccess.ReadWrite, , Len(s1))
         For x = 1 To (LOF(1) / Len(s1))
             FileGet(1, s1, x)
             Console.WriteLine("Member name is:" & s1.Name)
             Console.WriteLine("Member ID is:" & s1.MemID)
-            FileClose(1)
-
 
         Next
+        FileClose(1)
+        Console.ReadKey()
     End Sub
     Sub searchrec()
+        Dim recno As Integer
         Dim searchs As String
         Dim flagx As Boolean
 
         Console.WriteLine("enter name of member to be searched")
         searchs = Console.ReadLine()
 
-        FileOpen(1, "D:\memberdata", OpenMode.Random, Len(s1))
+        FileOpen(1, "E:\memberdata", OpenMode.Random, OpenAccess.ReadWrite, , Len(s1))
         recno = (LOF(1) / Len(s1))
         For x = 1 To recno
             FileGet(1, s1, x)
@@ -60,36 +69,39 @@
             Else
                 flagx = False
             End If
-            FileClose(1)
+
         Next
+        FileClose(1)
 
         If flagx = True Then
             Console.WriteLine("Member found. Corresponding ID is " & s1.MemID)
         Else
             Console.WriteLine("member not found")
         End If
-
+        Console.ReadKey()
     End Sub
     Sub addnewdata()
+        Dim recno As Integer
         Dim response As Char
         Console.WriteLine("do you wish to add data for new members? Enter Y/N")
         response = Console.ReadLine()
 
-        FileOpen(1, "D:\memberdata", OpenMode.Random, Len(s1))
+        FileOpen(1, "E:\memberdata", OpenMode.Random, OpenAccess.ReadWrite, , Len(s1))
+
         recno = LOF(1) / Len(s1)
 
         While response = "Y"
+            recno = recno + 1
+
             Console.WriteLine("enter name of new member")
             s1.Name = Console.ReadLine()
 
             Console.WriteLine("enter their ID")
             s1.MemID = Console.ReadLine()
 
-            FileOpen(1, "D:\memberdata", OpenMode.Random, Len(s1))
-            recno = recno + 1
 
             FilePut(1, s1, recno)
-            FileClose(1)
+
             Console.WriteLine("any more data to be added? Enter Y or N")
             response = Console.ReadLine()
 
@@ -97,25 +109,60 @@
         FileClose(1)
     End Sub
 
+    Sub additional()
+        '1.5'
+        Dim recno As Integer
+        Dim newno As Integer
+        Dim newname As String
+        Dim newID As Integer
+        Dim s2 As newmemrec
+        FileOpen(1, "E:\memberdata", OpenMode.Random, OpenAccess.ReadWrite, , Len(s1))
+        recno = LOF(1) / Len(s1)
+
+        FileOpen(2, "E:\memberdataEDIT", OpenMode.Random, OpenAccess.ReadWrite, , Len(s2))
+
+        For x = 1 To recno
+
+            FileGet(1, s1, x)
+            newname = s1.Name
+            newID = s1.MemID
+
+
+            Console.WriteLine("enter telephone number for member no. " & x)
+            s2.telenum = Console.ReadLine()
+            Console.WriteLine("Enter membership start date")
+            s2.startdate = Console.ReadLine()
+            s2.Name = newname
+            s2.MemID = newID
+
+
+            newno = (LOF(2) / Len(s2)) + 1
+            FilePut(2, s2, newno)
+
+        Next
+
+        FileClose(2)
+        FileClose(1)
+    End Sub
 
     Sub Main()
-        Dim recno As Integer = 0
         Dim newno As Integer = 0
-        Dim x As Integer
+
 
         Dim choice As Integer
         Dim choP As Int16
         choP = 0
-        Dim thing As Boolean
-        Dim recChoice As Integer
+
+
         choice = 0
-        While choice <> 5
+        While choice <> 6
             Console.Clear()
-            Console.WriteLine("Enter 1 to add 5 records to the random file")
+            Console.WriteLine("Enter 1 to add records to the random file")
             Console.WriteLine("Enter 2 to output all records from the random file")
-            Console.WriteLine("Enter 3 to add new member data")
+            Console.WriteLine("Enter 3 to append the file(Add data for more members)")
             Console.WriteLine("Enter 4 to search records")
-            Console.WriteLine("Enter 5 to exit")
+            Console.WriteLine("Enter 5 to add additional data for existing members")
+            Console.WriteLine("Enter 6 to exit")
             Console.Write("Your choice... ")
             choice = Console.ReadLine
 
@@ -128,8 +175,9 @@
 
                 Case 4 : Call searchrec()
 
-                Case 5
+                Case 5 : Call additional()
 
+                Case 6
 
                 Case Else
                     Console.WriteLine("wrong choice entered")
@@ -137,34 +185,6 @@
             End Select
         End While
 
-
-
-        '1.5'
-        Dim newname As String
-        Dim newID As Integer
-        FileOpen(1, "D:\memberdata", OpenMode.Random, Len(s1))
-        recno = LOF(1) / Len(s1)
-        FileClose(1)
-
-        For x = 1 To recno
-            FileOpen(1, "D:\memberdata", OpenMode.Random, Len(s1))
-            FileGet(1, s1, x)
-            newname = s1.Name
-            newID = s1.MemID
-            FileClose(1)
-            Dim s2 As newmemrec
-            Console.WriteLine("enter tele number for member no. " & x)
-            s2.telenum = Console.ReadLine()
-            Console.WriteLine("Enter membership start date")
-            s2.startdate = Console.ReadLine()
-            s2.Name = newname
-            s2.MemID = newID
-
-            FileOpen(2, "D:\memberdataEDIT", OpenMode.Random, Len(s2))
-            newno = (LOF(2) / Len(s2)) + 1
-            FilePut(1, s2, newno)
-            FileClose(2)
-        Next
 
 
 
